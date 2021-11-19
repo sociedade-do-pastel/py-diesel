e639b9c49df016348f9cd22dc7e5c72f49abf744
seleonel <leon_bellini@outlook.com>
2021-11-18 21:46:22 -0300

    Corrigida logica de hold do `secador_2.py`

--- secador_2.py
+++ secador_2.py
@@ -20,7 +20,7 @@ DRYER_DEDUCTION = 0.025
 
 app = FastAPI()
 database = SimpleDB("secador_2", volume_secador=0.0)
-
+hold = False
 
 class Secador2(BaseModel):
     volume_secador_2: float
@@ -29,16 +29,18 @@ class Secador2(BaseModel):
 @app.post("/secador_2/", status_code=200)
 def inserir_volume_secador(input_secador: Secador2, response: Response):
     global database
+    global hold
     if input_secador.volume_secador_2 >= 0:
         discounted_input = (input_secador.volume_secador_2 -
                             input_secador.volume_secador_2 * DRYER_DEDUCTION)
         try:
             database.begin_connection()
             database.increment("volume_secador", discounted_input)
+            hold = True
             return {"volume_secador_2": database.get("volume_secador")}
         finally:
             database.end_connection()
-
+            
     response.status_code = 400
     return dict()
 
@@ -54,23 +56,23 @@ def pegar_volume_secador():
 
 
 def hold():
+    global hold
     database.begin_connection()
     while True:
         liters = floor(database.get("volume_secador"))
-        if (liters > 0 and enviar_litros(liters)):
-            database.increment("volume_secador", -liters)
+        if (hold and liters > 0):
             time.sleep(liters * HOLD_DELAY)
+            if enviar_litros(liters):
+                database.increment("volume_secador", -liters)
+            hold = False
     database.end_connection()
 
 
 def enviar_litros(valor):
-    #########################################################
-    # sv_response = requests.post(                          #
-    #     f"http://127.0.0.1:{TANK_PORT}/tanque_biodiesel", #
-    #     json={"volume_secador_2": valor})                 #
-    # return (sv_response.status_code == 200)               #
-    #########################################################
-    return True
+    sv_response = requests.post(                          
+        f"http://127.0.0.1:{TANK_PORT}/tanque_biodiesel", 
+        json={"volume_secador_2": valor})                 
+    return (sv_response.status_code == 200)               
 
 
 if __name__ == "__main__":
